c19a56925e329d6bc2c4e0732072ebaa80062d8c
AlexeySerebryakov638 <alexijsilver@mail.ru>
2024-07-10 16:57:48 +0300

    Регистрация работников теперь работает

--- src/main/java/com/bookstore/security/SecurityConfig.java
+++ src/main/java/com/bookstore/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.bookstore.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManagerResolver;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,6 +15,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Configuration
 public class SecurityConfig {
@@ -41,8 +44,8 @@ public class SecurityConfig {
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(auth -> auth.requestMatchers("/").permitAll())
     		.authorizeHttpRequests(auth -> auth.requestMatchers("/user/**").permitAll())
-        	.authorizeHttpRequests(auth -> auth.requestMatchers("/cart/").authenticated())
-        	.authorizeHttpRequests(auth -> auth.requestMatchers("/employee/**").hasAnyRole("employee", "admin"))
+        	.authorizeHttpRequests(auth -> auth.requestMatchers("/cart").authenticated())
+        	.authorizeHttpRequests(auth -> auth.requestMatchers("/employee/**").hasAnyAuthority("employee", "admin"))
             .formLogin(login -> login.permitAll())
             .logout(logout -> logout.permitAll());
          
